5024c7cb93eeae63fd2ee667c5240efbcf909dcd
Rene Kustura <kusturarene@gmail.com>
2023-11-17 00:03:40 +0100

    Adds dataframe memory release

--- swiss_back/back_app.py
+++ swiss_back/back_app.py
@@ -3,6 +3,7 @@ from data_loader import read_data
 from data_driver import DocumentBase
 from dotenv import load_dotenv
 import os
+import gc
 
 load_dotenv()
 
@@ -22,9 +23,12 @@ def upload_excel():
             )
             db.add_authors(df1)
             db.add_sections(df2)
+            del df1
+            del df2
+            gc.collect()
             db.add_section_refs(df3)
 
             return jsonify({"message": "Data uploaded to Neo4j successfully"})
-        return jsonify({"error": f"Error: {str(e)}"})
+        return jsonify({"error": f"Error: Wrong file format."})
     except Exception as e:
         return jsonify({"error": f"Error: {str(e)}"})
